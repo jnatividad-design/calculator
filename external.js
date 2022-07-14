df3f777781e8fb7e0015bf04bfbe87027377a4ac
jnatividad-design <joshuamnatividad@yahoo.com>
2022-07-14 19:30:45 -0400

    Add index, js and styles files

---
+++ external.js
@@ -0,0 +1,28 @@
+
+function add(a,b) {
+parseFloat(a);
+parseFloat(b);
+let sum = (a+b)
+return sum
+}
+
+function subtract(a,b) {
+    parseFloat(a);
+    parseFloat(b);
+    let sub = (a-b)
+    return sub
+}
+
+function multiply(a,b) {
+    parseFloat(a);
+    parseFloat(b);
+    let product = (a*b)
+    return product
+}
+
+function divide(a,b){
+    parseFloat(a);
+    parseFloat(b);
+    let sum = (a/b)
+    return sum
+}
