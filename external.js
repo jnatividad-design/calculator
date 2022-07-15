5fd97a97fb4f9bf553c00b4d13068ca565521c81
jnatividad-design <joshuamnatividad@yahoo.com>
2022-07-15 18:41:31 -0400

    Fix HTML elements

--- external.js
+++ external.js
@@ -1,4 +1,28 @@
+const one = document.getElementById('1');
+const two = document.getElementById('2');
+const three = document.getElementById('3');
+const four = document.getElementById('4');
+const five = document.getElementById('5');
+const six = document.getElementById('6');
+const seven = document.getElementById('7');
+const eight = document.getElementById('8');
+const nine = document.getElementById('9');
+const zero = document.getElementById('0');
+const addition = document.getElementById('+');
+const subtraction = document.getElementById('-');
+const multiplication = document.getElementById('*');
+const division = document.getElementById('/');
+const gui = document.getElementById('gui');
+const calcContainer = document.getElementById('calcContainer')
 
+console.log(document.getElementById('gui') !==null)
+
+one.addEventListener(onclick, () => {
+document.querySelector('gui')
+gui.textContent ="1"
+})
+
+//Arithmetic functions
 function add(a,b) {
 parseFloat(a);
 parseFloat(b);
@@ -27,6 +51,7 @@ function divide(a,b){
     return sum
 }
 
+//Operator function
 function operate(a,b,operator) {
     switch (operator) {
         case "+":
